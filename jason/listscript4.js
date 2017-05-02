e06f60a50201ac48af42e8b43642b95acc0aa87b
Jason Huang <jasonhuang16@hotmail.com>
2017-05-02 11:54:43 -0700

    init commit jason branch

---
+++ jason/listscript4.js
@@ -0,0 +1,14 @@
+
+$(function () {
+    $("#btnAdd").bind("click", function () {
+        var div = $("<tr />");
+        div.html(GetDynamicTextBox(""));
+        $("#TextBoxContainer").append(div);
+    });
+    $("body").on("click", ".remove", function () {
+        $(this).closest("tr").remove();
+    });
+});
+function GetDynamicTextBox(value) {
+    return '<td><input name = "DynamicTextBox" type="text" value = "' + value + '" class="form-control" /></td>' + '' + '<td><input name = "DynamicTextBox" type="checkbox" value = "' + value + '" /></td>' + '<td><button type="button" class="btn btn-danger remove"><i class="glyphicon glyphicon-remove-sign"></i></button></td>'
+}
