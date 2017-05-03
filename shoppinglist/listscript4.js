d73c9fcc1ad06bf80bd5d10a57a798d8bb4e2a96
Karanveer Singh Khanna <kayvie2@gmail.com>
2017-05-03 15:15:22 -0700

    Removed unnecessary folder and files.

--- shoppinglist/listscript4.js
+++ shoppinglist/listscript4.js
@@ -17,7 +17,7 @@ $(function () {
 function GetDynamicTextBox(value) {
     count++;
     return '<td style="width:1px"><div class="check"><input id="'+count+'" type="checkbox"/><label for="'+count+'"><div class="box"><i class="fa fa-check"></i></div></label></div></td>'
-    + '<td><input name = "DynamicTextBox" type="text" value = "' + value + '" class="form-control" /></td>' 
+    +'<td><input name = "DynamicTextBox" type="text" value = "' + value + '" class="form-control" /></td>' 
     + '<td><input name = "DynamicTextBox" type="text" value = "' + value + '" size ="1"  class="form-control" /></td>' 
     + '<td  style="width:1px;"><button type="button" class="btn btn-danger remove"><i class="glyphicon glyphicon-remove-sign"></i></button></td>'
 }
