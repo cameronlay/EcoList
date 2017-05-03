379349489c2c6d3c4a9431fa4ddf33170a0f136c
Jason Huang <jasonhuang16@hotmail.com>
2017-05-03 15:02:53 -0700

    small changes

--- jason/listscript4.js
+++ jason/listscript4.js
@@ -1,4 +1,6 @@
 
+var count =0;
+
 $(function () {
     $("#btnAdd").bind("click", function () {
         var div = $("<tr />");
@@ -7,11 +9,15 @@ $(function () {
         $("#TextBoxContainer").append(div);
     });
     $("body").on("click", ".remove", function () {
-        $(this).closest("tr").fadeTo(600,0.4, function(){
+        $(this).closest("tr").fadeTo(280,0.4, function(){
             $(this).remove();
         })
     });
 });
 function GetDynamicTextBox(value) {
-    return '<td><input name = "DynamicTextBox" type="text" value = "' + value + '" class="form-control" /></td>' + '<td><input name = "DynamicTextBox" type="text" value = "' + value + '" class="form-control" /></td>' + '<td><input name = "DynamicTextBox" type="checkbox" value = "' + value + '" /></td>' + '<td><button type="button" class="btn btn-danger remove"><i class="glyphicon glyphicon-remove-sign"></i></button></td>'
+    count++;
+    return '<td style="width:1px"><div class="check"><input id="'+count+'" type="checkbox"/><label for="'+count+'"><div class="box"><i class="fa fa-check"></i></div></label></div></td>'
+    + '<td><input name = "DynamicTextBox" type="text" value = "' + value + '" class="form-control" /></td>' 
+    + '<td><input name = "DynamicTextBox" type="text" value = "' + value + '" size ="1"  class="form-control" /></td>' 
+    + '<td  style="width:1px;"><button type="button" class="btn btn-danger remove"><i class="glyphicon glyphicon-remove-sign"></i></button></td>'
 }
