02706956ccd94e664dc45985b2db5222d41b14bc
Karanveer Singh Khanna <kayvie2@gmail.com>
2017-05-05 11:30:37 -0700

    Shoping list button fixed.

--- jason/listscript4.js
+++ jason/listscript4.js
@@ -1,5 +1,7 @@
 
 var count =0;
+var c = 0;
+var a;
 
 $(function () {
     //button adds new rows to table 1
@@ -21,7 +23,9 @@ $(function () {
     $(document).ready(
     function(){
         $("body").on("click",".list", function (){
-                var toAdd = $('input[name=DynamicTextBox]').val();
+                var id = $(this).attr('id');
+                a = parseInt(id);
+                var toAdd = $("#" + a).val();
                  $('ol').append('<li>' + toAdd + '</li>');
                  $(this).closest("tr").remove();
             });
@@ -45,8 +49,6 @@ $(function () {
     }
 );
 
-
-
     //removes all rows 
     $("#btnRemove").on("click", function () {
             $("#TextBoxContainer").children().remove();
@@ -57,8 +59,9 @@ $(function () {
 
 function GetDynamicTextBox(value) {
     count++;
-    return '<td><button type="button" class="btn btn-info list"><span class="glyphicon glyphicon-shopping-cart"></span></button></td>'
-    +'<td><input name = "DynamicTextBox" type="text" value = "' + value + '" class="form-control" /></td>' 
+    c++;
+    return '<td><button type="button" id="'+c+'btn" class="btn btn-info list"><span class="glyphicon glyphicon-shopping-cart"></span></button></td>'
+    +'<td><input name = "DynamicTextBox" id="'+count+'" type="text" value = "' + value + '" class="form-control" /></td>' 
     + '<td><input name = "DynamicTextBox" type="text" value = "' + value + '"  class="form-control" /></td>' 
     + '<td><button type="button" class="btn btn-danger remove"><i class="glyphicon glyphicon-remove-sign"></i></button></td>'
 }
