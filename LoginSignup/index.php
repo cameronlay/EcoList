b053cfb980c87a3785ac0f144b2d3279184a9036
Cameron Lay <cameron.lay1@gmail.com>
2017-05-10 17:31:29 -0700

    Added burger nav to sitemap

--- LoginSignup/index.php
+++ LoginSignup/index.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <title>EcoList - Login</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Cameron Lay">
+    <link rel="shortcut icon" href="/LOGO/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/LOGO/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/css/Burgers.css"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="script.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="style.css">
-    <script src="script.js"></script>
+    <script src="/javascript/hamburger.js"></script>
+    <script src="/javascript/functionHiding.js" id="SelfDestruct"></script>
 </head>
 <body onload="setBurger(true);hide_function()">
     <div id="GUI">
