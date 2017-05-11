7dca7457955118b968080da010367bdd89685925
Karanveer Singh Khanna <kayvie2@gmail.com>
2017-05-11 16:31:25 -0700

    Added images on app page. Integrated hamburger menu.

--- AppsHome/appshome.php
+++ AppsHome/appshome.php
@@ -3,13 +3,20 @@
 		<meta charset="UTF-8"/>
 		<title>Apps Home</title>
 		<link rel="stylesheet" href="css/appshome.css"/>
+        <link rel="stylesheet" href="../Burgers.css"/>
+        <script src="../hamburger.js"></script>
 	</head>
 	<body>
-		<div id="GUI"></div>
+		<div id="GUI">
+            <?php
+                include('../burgerMenuGUI.html');
+            ?>
+        </div>
 		<div class="wrapper">
 			<div id="AppsHomeHeader">
 				<img src="../LOGO/Logo2.png" alt="EcoList"></img>
 			</div>
+            
 			<!--our app start-->
 			<div class="appsContent">
 			<h2>Our Apps</h2>
@@ -18,8 +25,8 @@
 					<div class="content">
 						<table>
 							<tr>
-							<td><img src="imagenotfound" alt="appicon" width=500 height=500/></td>
-							<td><h2>APPNAME</h2><p>
+							<td><img src="../images/cutlery1.png" alt="appicon" width=500 height=500/></td>
+							<td><h2>Shopping List</h2><p>
 							desc.
 							</p>
 							</td>
@@ -33,8 +40,8 @@
 					<div class="content">
 						<table>
 							<tr>
-							<td><img src="imagenotfound" alt="appicon" width=500 height=500/></td>
-							<td><h2>APPNAME</h2><p>
+							<td><img src="../images/calandar1.png" alt="appicon" width=500 height=500/></td>
+							<td><h2>EcoEats</h2><p>
 							desc.
 							</p>
 							</td>
@@ -44,12 +51,12 @@
 				</div>
 			<!--end of single app -->
 			<!-- single app-->
-				<div class="app">
+				<div class="app" id="w">
 					<div class="content">
 						<table>
 							<tr>
-							<td><img src="imagenotfound" alt="appicon" width=500 height=500/></td>
-							<td><h2>APPNAME</h2><p>
+							<td><img src="../images/location1.png" alt="appicon" width=500 height=500/></td>
+							<td><h2>Donation</h2><p>
 							desc.
 							</p>
 							</td>
@@ -60,6 +67,7 @@
 			<!--end of single app -->
 			</div>
 			<!--end of our apps-->
+            
 			<!--start of our friends-->
 			<div class="appsContent">
 			<h2>Our Friends</h2>
@@ -78,21 +86,7 @@
 					</div>
 				</div>
 			<!--end of single app -->
-			<!-- single app-->
-				<div class="app">
-					<div class="content">
-						<table>
-							<tr>
-							<td><img src="friends/RefrigdateLogo.png" alt="appicon" width=500 height=500/></td>
-							<td><h2>Refrigdate</h2><p>
-							If you are worried about keeping track of your leftovers and how old they may be we highly recommend checking out our friends at Refrigedate. Their app tracks how long your it has been since you stored your leftovers and lets you assign ownership to your food if you find your roommates are often eating your leftovers!
-							</p>
-							</td>
-							</tr>
-						</table>
-					</div>
-				</div>
-			<!--end of single app -->
+			
 			<!-- single app-->
 				<div class="app">
 					<div class="content">
@@ -101,6 +95,21 @@
 							<td><img src="friends/Take2Logo.png" alt="appicon" width=500 height=500/></td>
 							<td><h2>Take2</h2><p>
 							If you have any excess ingredients you can head over to our friends at Take 2. Their app allows you to input two ingredients and they will try to give you recipe recommendations that incorporates both of those ingredients.
+							</p>
+							</td>
+							</tr>
+						</table>
+					</div>
+				</div>
+			<!--end of single app -->
+            <!-- single app-->
+				<div class="app">
+					<div class="content">
+						<table>
+							<tr>
+							<td><img src="friends/RefrigdateLogo.png" alt="appicon" width=500 height=500/></td>
+							<td><h2>Refrigdate</h2><p>
+							If you are worried about keeping track of your leftovers and how old they may be we highly recommend checking out our friends at Refrigedate. Their app tracks how long your it has been since you stored your leftovers and lets you assign ownership to your food if you find your roommates are often eating your leftovers!
 							</p>
 							</td>
 							</tr>
