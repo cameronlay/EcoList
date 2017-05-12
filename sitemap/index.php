ead2536b2537af69b1094ab5f6b05b419ae7b035
Cameron Lay <cameron.lay1@gmail.com>
2017-05-11 23:15:57 -0700

    Updated links on sitemap and added burger menus

--- sitemap/index.php
+++ sitemap/index.php
@@ -5,19 +5,22 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="author" content="Cameron Lay">
-		<link rel="shortcut icon" href="/LOGO/favicon.ico" type="image/x-icon">
-		<link rel="icon" href="/LOGO/favicon.ico" type="image/x-icon">
+		<link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
+		<link rel="icon" href="/images/favicon.ico" type="image/x-icon">
         <link rel="stylesheet" href="style.css">
-		<link rel="stylesheet" href="/css/Burgers.css"/>
+		<link rel="stylesheet" href="/navMenu.css"/>
+        <link rel="stylesheet" href="/navButton.css"/>
+        <link rel="stylesheet" href="../fonts/font.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<script src="/javascript/hamburger.js"></script>
-		<script src="/javascript/functionHiding.js" id="SelfDestruct"></script>
+		<script src="/navScript.js"></script>
+		<script src="/functionHiding.js" id="SelfDestruct"></script>
     </head>
 	<body onload="setBurger(true);hide_function()">
 		<div id="GUI">
-			<?php include('burgerMenuGUI.html');?>
+			<?php include('navMenu.html');?>
+            <?php include('navButton.html');?>
 		</div>
 		<div class="container">
 			<div class="row">
@@ -29,25 +32,26 @@
 				<div class="row" >
 					<div class="col-med-4"></div>
 					<div class="siteMapBlock col-med-3">
-						<h2><a href="#">Home</a></h2>
+                        <h2><a href="/users/index.php">Login/Sign-Up</a></h2>
+						<h2><a href="/index.php">Home</a></h2>
 						<ul>
-							<li><a href="#">About Project</a></li>
-							<li class="alt"><a href="#">Why it is Important</a></li>
-							<li><a href="#">About Us</a></li>
-							<li class="alt"><a href="#">Affiliated Apps</a></li>
+							<li><a href="/index.php#OurPurpose">Our Purpose</a></li>
+							<li class="alt"><a href="/index.php#OurApps">Our Apps</a></li>
+							<li><a href="/index.php#AboutUs">About us</a></li>
+							<li class="alt"><a href="/index.php#AboutProject">About the Project</a></li>
 						</ul>
 					</div>
 				</div>
 				<div class="row">
 					<div class="col-med-4"></div>
 					<div class="siteMapBlock col-med-3">
-						<h2><a href="#">Shopping List</a></h2>
+						<h2><a href="/ecolist/index.php">Shopping List</a></h2>
 					</div>
 				</div>
 				<div class="row">
 					<div class="col-med-4"></div>
 					<div class="siteMapBlock col-med-3">
-						<h2><a href="#">Meal Plans</a></h2>
+						<h2><a href="/ecoeats/index.php">Meal Plans</a></h2>
 					</div>
 				</div>
 			</div>
