2d346d6a1816a69605713f05253cacf382f5461a
Karanveer Singh Khanna <kayvie2@gmail.com>
2017-05-12 13:15:20 -0700

    Commented the documents

--- eco-list/donation/index.php
+++ eco-list/donation/index.php
@@ -17,11 +17,11 @@
     </head>
     
     <body>
-
-    <div id="GUI">
-        <?php include('../navMenu.html');?>
-        <?php include('../navButton.html');?>
-    </div>
+        <!-- hamburger menu -->
+        <div id="GUI">
+            <?php include('../navMenu.html');?>
+            <?php include('../navButton.html');?>
+        </div>
         
     
         <header>
@@ -29,60 +29,63 @@
         </header>
         
         <main>
-        <div class="row">
-            <div class="col-sm-16">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae lobortis est. Sed mattis pretium nibh, quis sodales eros elementum ornare. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum elit urna, ornare vel aliquet eget, laoreet non lectus. Pellentesque vulputate ultrices leo eu pharetra. Duis rhoncus arcu enim, et ornare ipsum aliquam id. Aliquam ante magna, pretium in ante blandit, vehicula porttitor felis. Mauris a libero mollis, luctus augue non, tempor turpis. Fusce eget hendrerit leo, vel semper erat. Fusce convallis finibus velit non sollicitudin.
-            </div>
-        </div>
-                <div class="row">     
-                    <div class="col-sm-4" id="sideList">
-                        <p onmouseover="bounceMarker('m')" onmouseout="stopBounce()" class="well well-sm" id="1"><b>Surrey Food bank society</b><br>10732 City Pkwy<br>Surrey, BC V3T 4C7</p>
-                        <p onmouseover="bounceMarker('m1')" onmouseout="stopBounce()" class="well well-sm" ><b>Covenant House</b><br>575 Drake St<br>Vancouver, BC V6B 4K8</p>
-                        <p onmouseover="bounceMarker('m2')" onmouseout="stopBounce()" class="well well-sm" ><b>Langley Food Bank</b><br>5768 203 St<br>Langley, BC V3A 1W3</p>
-                        <p onmouseover="bounceMarker('m3')" onmouseout="stopBounce()" class="well well-sm" ><b>Quest Food Exchange</b><br>7753 Sixth St<br>New Westminster, BC V3L 3C8</p>
-                        <p onmouseover="bounceMarker('m4')" onmouseout="stopBounce()" class="well well-sm" ><b>Quest Outreach Society</b><br>346 E Hastings St<br>Vancouver, BC V6A 1P4</p>
-                        <p onmouseover="bounceMarker('m5')" onmouseout="stopBounce()" class="well well-sm" ><b>Richmond Food Bank Society</b><br>5800 Cedarbridge Way #100<br>Richmond, BC V6X 2A7</p>
-                        <p onmouseover="bounceMarker('m')" onmouseout="stopBounce()" class="well well-sm" ><b>Surrey Food bank society</b><br>10732 City Pkwy<br>Surrey, BC V3T 4C7</p>
-                    </div>
-                    <div class="col-sm-8" id="googleMap">
-                        <div id="map" style="height:400px;"></div>
-                        <script src="script.js"></script>
-                        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB1ypP9TWn_8TSLnbX3PyCCyE9jc_eFS7A&callback=myMap"></script>
-                    </div>
+            <!-- Place holder for content -->
+            <div class="row">
+                <div class="col-sm-16">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae lobortis est. Sed mattis pretium nibh, quis sodales eros elementum ornare. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum elit urna, ornare vel aliquet eget, laoreet non lectus. Pellentesque vulputate ultrices leo eu pharetra. Duis rhoncus arcu enim, et ornare ipsum aliquam id. Aliquam ante magna, pretium in ante blandit, vehicula porttitor felis. Mauris a libero mollis, luctus augue non, tempor turpis. Fusce eget hendrerit leo, vel semper erat. Fusce convallis finibus velit non sollicitudin.
                 </div>
-                </main>
-    <div class="home-footer mobileContentless">
-        <div class="footerconstraint">
-            <div class="bottomleftcolumn">
-                <ul>
-                    <li><a href="/index.php">Home</a></li>
-                    <li><a href="/index.php#OurPurpose">Our Purpose</a></li>
-                    <li><a href="/index.php#OurApps">About our Apps</a></li>
+            </div>
+            <div class="row"> 
+                <!-- Addresses for places -->
+                <div class="col-sm-4" id="sideList">
+                    <p onmouseover="bounceMarker('m')" onmouseout="stopBounce()" class="well well-sm" id="1"><b>Surrey Food bank society</b><br>10732 City Pkwy<br>Surrey, BC V3T 4C7</p>
+                    <p onmouseover="bounceMarker('m1')" onmouseout="stopBounce()" class="well well-sm" ><b>Covenant House</b><br>575 Drake St<br>Vancouver, BC V6B 4K8</p>
+                    <p onmouseover="bounceMarker('m2')" onmouseout="stopBounce()" class="well well-sm" ><b>Langley Food Bank</b><br>5768 203 St<br>Langley, BC V3A 1W3</p>
+                    <p onmouseover="bounceMarker('m3')" onmouseout="stopBounce()" class="well well-sm" ><b>Quest Food Exchange</b><br>7753 Sixth St<br>New Westminster, BC V3L 3C8</p>
+                    <p onmouseover="bounceMarker('m4')" onmouseout="stopBounce()" class="well well-sm" ><b>Quest Outreach Society</b><br>346 E Hastings St<br>Vancouver, BC V6A 1P4</p>
+                    <p onmouseover="bounceMarker('m5')" onmouseout="stopBounce()" class="well well-sm" ><b>Richmond Food Bank Society</b><br>5800 Cedarbridge Way #100<br>Richmond, BC V6X 2A7</p>
+                    <p onmouseover="bounceMarker('m')" onmouseout="stopBounce()" class="well well-sm" ><b>Surrey Food bank society</b><br>10732 City Pkwy<br>Surrey, BC V3T 4C7</p>
+                </div>
+                <div class="col-sm-8" id="googleMap">
+                    <div id="map" style="height:400px;"></div>
+                    <script src="script.js"></script>
+                    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB1ypP9TWn_8TSLnbX3PyCCyE9jc_eFS7A&callback=myMap"></script>
+                </div>
+            </div>
+        </main>
+        <!-- Footer-->
+        <div class="home-footer mobileContentless">
+            <div class="footerconstraint">
+                <div class="bottomleftcolumn">
+                    <ul>
+                        <li><a href="/index.php">Home</a></li>
+                        <li><a href="/index.php#OurPurpose">Our Purpose</a></li>
+                        <li><a href="/index.php#OurApps">About our Apps</a></li>
 
-                </ul>
+                    </ul>
+                </div>
+                <div class="middleleftcolumn">
+                    <ul>
+                        <li><a href="/ecolist/index.php">Shopping List</a></li>
+                        <li><a href="/ecoeats/index.php">Meal Planner</a></li>
+                        <li><a href="/donation/index.php">Donate Food</a></li>
+                    </ul>
+                </div>
+                <div class="middlerightcolumn">
+                    <ul>
+                        <li><a href="/services/index.php">Affiliated Apps</a></li>
+                        <li><a href="/index.php#AboutUs">About Us</a></li>
+                        <li><a href="/index.php#AboutProject">About the Project</a></li>
+                    </ul>
+                </div>
+                <div class="bottomrightcolumn">
+                    <ul>
+                        <li><a href="/users/index.php">Login</a></li>
+                        <li><a href="/sitemap/index.php">Sitemap</a></li>
+                    </ul>
+                </div>
+                <p>&copy; Copyright 2017 | EcoList, All Rights Reserved </p>
             </div>
-            <div class="middleleftcolumn">
-                <ul>
-                    <li><a href="/ecolist/index.php">Shopping List</a></li>
-                    <li><a href="/ecoeats/index.php">Meal Planner</a></li>
-                    <li><a href="/donation/index.php">Donate Food</a></li>
-                </ul>
-            </div>
-            <div class="middlerightcolumn">
-                <ul>
-                    <li><a href="/services/index.php">Affiliated Apps</a></li>
-                    <li><a href="/index.php#AboutUs">About Us</a></li>
-                    <li><a href="/index.php#AboutProject">About the Project</a></li>
-                </ul>
-            </div>
-            <div class="bottomrightcolumn">
-                <ul>
-                    <li><a href="/users/index.php">Login</a></li>
-                    <li><a href="/sitemap/index.php">Sitemap</a></li>
-                </ul>
-            </div>
-            <p>&copy; Copyright 2017 | EcoList, All Rights Reserved </p>
         </div>
-    </div>
     </body>
 </html>
--- eco-list/donation/script.js
+++ eco-list/donation/script.js
@@ -1,12 +1,20 @@
+// displaying position on maps
 var marker;
 var marker1;
 var marker2;
+var marker3;
+var marker4;
+var marker5;
+
+// timer for bouncing the marker
 var bounceTimer;
 var count = 2;
 
+
+// Setting the whole map
 function myMap() {
     
-    //maps
+    // map with center at given latitude and longitude
     var myCenter = new google.maps.LatLng(49.209631, -122.928385);
     var mapCanvas = document.getElementById("map");
     var mapOptions = {
@@ -14,49 +22,53 @@ function myMap() {
         zoom: 10,
     };
     var map = new google.maps.Map(mapCanvas, mapOptions);
-            
-    //surrey
+    
+    
+    //marker on surrey food bank
     var myCenter1 = new google.maps.LatLng(49.1977, -122.8487);
     marker = new google.maps.Marker({
         position:myCenter1,
         optimized:false
     });
     
-    //covenant house
+    //marker on covenant house
     var myCenter2 = new google.maps.LatLng(49.2754,-123.1265);
     marker1 = new google.maps.Marker({
         position:myCenter2,
         optimized:false
     });
     
-    // langley
+    //marker on langley food bank
     var myCenter3 = new google.maps.LatLng(49.1074, -122.6596);
     marker2 = new google.maps.Marker({
         position:myCenter3,
         optimized:false
     });
     
-    // quest food exchange
+    //marker on quest food exchange
     var myCenter4 = new google.maps.LatLng(49.2207, -122.9303);
     marker3 = new google.maps.Marker({
         position:myCenter4,
         optimized:false
     });
     
-    // outreach society
+    //marker on outreach society
     var myCenter5 = new google.maps.LatLng(49.281123, -123.096309);
     marker4 = new google.maps.Marker({
         position:myCenter5,
         optimized:false
     });
     
-    // richmond food bank
+    //marker on richmond food bank
     var myCenter6 = new google.maps.LatLng(49.172892, -123.141725);
     marker5 = new google.maps.Marker({
         position:myCenter6,
         optimized:false
     });
     
+    
+    /* This makes the marker bounce, when we hover over markers. SetAnimation() function set the 
+       markers to bounce after every 500 millisec.*/
     google.maps.event.addListener(marker, 'mouseover', function() {
         if (this.getAnimation() == null || typeof this.getAnimation() === 'undefined') {
             
@@ -70,12 +82,14 @@ function myMap() {
         }
     });
     
+    /* This helps to zoom the map when ckicked on markers.*/
     google.maps.event.addListener(marker,'dblclick',function() {
         map.setZoom(map.getZoom() + count);
         map.setCenter(marker.getPosition());
     });
     
     
+    /* This stop the marker to bounce when we mouse out. */
     google.maps.event.addListener(marker, 'mouseout', function() {
         
         if (this.getAnimation() != null) {
@@ -214,7 +228,7 @@ function myMap() {
         clearTimeout(bounceTimer);
     });
 
-    
+    // Officially setting the map with markers on it.
     marker.setMap(map);
     marker1.setMap(map);
     marker2.setMap(map);
@@ -227,7 +241,7 @@ function myMap() {
 
 
 
-
+// this function makes the marker to bounce when we hover over the addresses.
 
 function bounceMarker(x) {
     if (x == 'm') {
@@ -276,7 +290,7 @@ function bounceMarker(x) {
     }
 }
 
-
+// this function makes markers stop bouncing when we mouseout the cursor from the address
 function stopBounce() {
     
     if (marker.getAnimation() != null) {
