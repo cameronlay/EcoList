566070d72b22a2d12d42262c62187bdd25f7d5de
Karanveer Singh Khanna <kayvie2@gmail.com>
2017-05-11 17:32:58 -0700

    Donation page.

--- templateDonation/index.php
+++ templateDonation/index.php
@@ -4,6 +4,9 @@
         <title>Donation</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="style.css">
         <link rel="stylesheet" href="../Burgers.css"/>
         <script src="../hamburger.js"></script>
@@ -19,20 +22,13 @@
         
     
         <header>
-                <h1>Donate here.!</h1>
+                <h1>Donate Today</h1>
         </header>
         
         <main>
-            <p>
+        <div class="row">
+            <div class="col-sm-16">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae lobortis est. Sed mattis pretium nibh, quis sodales eros elementum ornare. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum elit urna, ornare vel aliquet eget, laoreet non lectus. Pellentesque vulputate ultrices leo eu pharetra. Duis rhoncus arcu enim, et ornare ipsum aliquam id. Aliquam ante magna, pretium in ante blandit, vehicula porttitor felis. Mauris a libero mollis, luctus augue non, tempor turpis. Fusce eget hendrerit leo, vel semper erat. Fusce convallis finibus velit non sollicitudin.
-            </p>
-            
-           
-              
-            <div>
-                <div id="map" style="width:100%;height:400px"></div>
-                <script src="script.js"></script>
-                <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB1ypP9TWn_8TSLnbX3PyCCyE9jc_eFS7A&callback=myMap"></script>
             </div>
         </div>
                 <div class="row">     
