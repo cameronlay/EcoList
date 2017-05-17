a80e31576d4cfc2f389c1b52cce8e0ed1b9d5d11
Cameron Lay <cameron.lay1@gmail.com>
2017-05-17 14:29:34 -0700

    continued testing

--- eco-list/ecolist/script.js
+++ eco-list/ecolist/script.js
@@ -118,4 +118,11 @@ function GetDynamicTextBox(value) {
     + '<td><button type="button" class="btn btn-danger remove"><i class="glyphicon glyphicon-minus-sign"></i></button></td>'
 }
 
-//mobile swipe delete function
+
+function dbUpdate(){
+    if(c !== 0){
+        for (i = 0; i <= count; i++){
+
+        }
+    }
+}
--- eco-list/public/ecolist/dbinput.js
+++ eco-list/public/ecolist/dbinput.js
@@ -1,8 +1,5 @@
-(function () {
-
-
 // Initialize Firebase
-    var config = {
+var config = {
         apiKey: "AIzaSyCFV9ueKtaYkvl14ZkklYtHpdXJJ-ODpiI",
         authDomain: "ecolist-bc2f6.firebaseapp.com",
         databaseURL: "https://ecolist-bc2f6.firebaseio.com",
@@ -10,38 +7,5 @@
         storageBucket: "ecolist-bc2f6.appspot.com",
         messagingSenderId: "954845578727"
     };
-    var firebase = new Firebase('https://ecolist-bc2f6.firebaseio.com');
-    firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-// get elements
-
-    const listObject = document.getElementById('listContainer');
-    const cartList = document.getElementById('cartItems');
-
-// create references
-    const dbRefList = firebase.database().ref().child('list');
-    const dbRefCart = dbRefList.child('cartItems');
-
-// sync object changes
-    dbRefList.on('value', function(snap) {
-        listObject.innerText = JSON.stringify(snap.val(), null, 1)
-    });
-
-    //sync list changes
-    dbRefCart.on('child_added', function(snap) {
-        const li = document.createElement('li');
-        li.innerText = snap.val();
-        li.id = snap.key;
-        cartList.appendChild(li);
-    });
-
-    dbRefCart.on('child_changed', function(snap) {
-        const liChanged = document.getElementById(snap.key);
-        liChanged.innerText = snap.val();
-    });
-
-    dbRefCart.on('child_removed', function(snap) {
-        const liToRemove = document.getElementById(snap.key);
-        liToRemove.remove();
-    });
-});
