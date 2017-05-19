314ce248d08ab2bcc1e4aea58828c349c9131a2b
Cameron Lay <cameron.lay1@gmail.com>
2017-05-19 12:53:23 -0700

    deleting excess files

--- eco-list/public/functionHiding.js
+++
@@ -1,5 +0,0 @@
-//remove functions from body when loading the page to hide functions in inspector
-function hide_function() {
-	document.getElementsByTagName("body")[0].removeAttribute("onLoad");
-	document.getElementById("SelfDestruct").remove();
-}
