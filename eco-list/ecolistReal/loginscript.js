f5492081f8f31973db4d2e79f15eba673b0cb9a7
Karanveer Singh Khanna <kayvie2@gmail.com>
2017-05-23 15:35:25 -0700

    Working on bugs

--- eco-list/ecolistReal/loginscript.js
+++ eco-list/ecolistReal/loginscript.js
@@ -1,4 +1,6 @@
-// Initialize Firebase
+
+var winSize = window.innerWidth;
+
 var config = {
     apiKey: "AIzaSyC82ZRbf--52RkwFWRCvCBRN7zLSMCAqV8",
     authDomain: "ecolist-d3e23.firebaseapp.com",
@@ -13,35 +15,27 @@ firebase.auth().onAuthStateChanged( function() {
 	const user = firebase.auth().currentUser;
 		if(user != null) {
 			firebase.database().ref("users/" + user.uid).set({name:user.displayName});
-		} 
-        else 
+            
+
+            
+            document.getElementById("logIn").style.display="none";
+            document.getElementById("logOut").style.display="block";
+            if(winSize > 786) {
+                document.getElementById("p").style.display = "block";
+                document.getElementById("p").innerHTML = "Welcome, " + user.displayName;
+            } else if(winSize < 786) {
+                document.getElementById("p").style.display = "none";
+                document.getElementById("t").innerHTML = "Hey, " + user.displayName;
+            }
+        }            
+        else {
             console.log("not signed in");
+            document.getElementById("logOut").style.display="none";
+            document.getElementById("logIn").style.display="block";
+        }
 			
 });
         
 function logOut() {
     firebase.auth().signOut();
 }
-        
-var winSize = window.innerWidth;
-    
-firebase.auth().onAuthStateChanged(function(user) {
-  
-if (user) {
-    document.getElementById("logIn").style.display="none";
-    document.getElementById("logOut").style.display="block";
-    console.log("signin");
-} else {
-    document.getElementById("logOut").style.display="none";
-    document.getElementById("logIn").style.display="block";
-    console.log("out");
-}
- 
-if(user && winSize > 786) {
-    document.getElementById("p").style.display = "block";
-    document.getElementById("p").innerHTML = "Welcome, " + user.displayName;
-} else if(user && winSize < 786) {
-    document.getElementById("p").style.display = "none";
-    document.getElementById("t").innerHTML = "Hey, " + user.displayName;
-}  
-});
