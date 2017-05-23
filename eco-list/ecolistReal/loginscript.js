f8bde050013e085744d7533c4aba943e60d1bde5
Karanveer Singh Khanna <kayvie2@gmail.com>
2017-05-23 15:54:38 -0700

    Bug fixed

--- eco-list/ecolistReal/loginscript.js
+++ eco-list/ecolistReal/loginscript.js
@@ -10,28 +10,43 @@ var config = {
     messagingSenderId: "347349975058"
 };
 firebase.initializeApp(config);
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    user.updateProfile({
+        displayName: ""
+    });
+
+  } else {
+    // No user is signed in.
+  }
+});
+
+
+
   
 firebase.auth().onAuthStateChanged( function() {
 	const user = firebase.auth().currentUser;
 		if(user != null) {
 			firebase.database().ref("users/" + user.uid).set({name:user.displayName});
-            
-
-            
             document.getElementById("logIn").style.display="none";
             document.getElementById("logOut").style.display="block";
-            if(winSize > 786) {
-                document.getElementById("p").style.display = "block";
-                document.getElementById("p").innerHTML = "Welcome, " + user.displayName;
-            } else if(winSize < 786) {
-                document.getElementById("p").style.display = "none";
-                document.getElementById("t").innerHTML = "Hey, " + user.displayName;
-            }
+            
         }            
         else {
             console.log("not signed in");
             document.getElementById("logOut").style.display="none";
             document.getElementById("logIn").style.display="block";
+        }
+        
+        
+        if(user && winSize > 786) {
+                document.getElementById("p").style.display = "block";
+                document.getElementById("p").innerHTML = "Welcome, " + user.displayName;
+        } else if(user && winSize < 786) {
+                document.getElementById("p").style.display = "none";
+                document.getElementById("t").innerHTML = "Hey, " + user.displayName;
         }
 			
 });
